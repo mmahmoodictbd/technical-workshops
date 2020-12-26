2bc067d26056117e7c1bb31ac61c85da33f1632b
Mossaddeque Mahmood <mmahmood.ict.bd@gmail.com>
2020-12-27 00:05:29 +0100

    Update maven intro project.

--- java-maven-basics/hello-world/src/main/java/com/unloadbrain/App.java
+++ java-maven-basics/hello-world/src/main/java/com/unloadbrain/App.java
@@ -1,13 +1,25 @@
 package com.unloadbrain;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
+import java.io.IOException;
+import java.net.URL;
+
+public class App {
+
+	public static void main(String[] args) {
+
+		System.out.println("Hello World!");
+
+		Document prothomAloPage;
+		try {
+			prothomAloPage = Jsoup.parse(new URL("https://www.prothomalo.com/"), 10000);
+		} catch (IOException e) {
+			throw new RuntimeException("Could not download document.", e);
+		}
+
+		System.out.println(prothomAloPage.select("#header time").text());
+	}
 }
